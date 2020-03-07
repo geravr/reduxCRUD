e078aaef5666c23d9006740cbe109ea7198d67d2
geravr <gera.villa21@gmail.com>
2020-03-07 13:36:10 -0600

    update NewProduct Component

--- src/components/NewProduct.js
+++ src/components/NewProduct.js
@@ -2,7 +2,43 @@ import React from 'react';
 
 const NewProduct = () => {
     return ( 
-        <h1>Nuevo Producto</h1>
+        <div className="row justify-content-center">
+            <div className="col-md-8">
+                <div className="card">
+                    <div className="card-body">
+                        <h2 className="text-center mb-4 font-weight-bold">
+                            Agregar nuevo producto
+                        </h2>
+                        <form>
+                            <div className="form-group">
+                                <label> Nombre de producto</label>
+                                <input 
+                                    type="text"
+                                    className="form-control"
+                                    placeholder="Nombre de producto"
+                                    name="name"
+                                />
+                            </div>
+                            <div className="form-group">
+                                <label> Precio  de producto</label>
+                                <input 
+                                    type="number"
+                                    className="form-control"
+                                    placeholder="Precio de producto"
+                                    name="price"
+                                />
+                            </div>
+                            <button
+                                type="submit"
+                                className="btn btn-primary font-weight-bold text-uppercase d-block w-100"
+                            >
+                                Agregar
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
      );
 }
  
