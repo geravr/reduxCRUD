baf763612397948b85c7d4f282a7769c1a21df9b
geravr <gera.villa21@gmail.com>
2020-03-07 18:11:04 -0600

    add Redux to the project

--- src/App.js
+++ src/App.js
@@ -6,17 +6,26 @@ import EditProduct from './components/EditProduct';
 
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+// REDUX
+import { Provider } from 'react-redux';
+import store from './store';
+
 function App() {
   return (
     <Router>
-      <Header />
-      <div className="container mt-5">
-        <Switch>
-          <Route exact path="/" component={Products} />
-          <Route exact path="/product/new" component={NewProduct} />
-          <Route exact path="/product/edit/:id" component={EditProduct} />
-        </Switch>
-      </div>
+    
+      {/*REDUX*/}
+      <Provider store={store}>
+
+        <Header />
+        <div className="container mt-5">
+          <Switch>
+            <Route exact path="/" component={Products} />
+            <Route exact path="/product/new" component={NewProduct} />
+            <Route exact path="/product/edit/:id" component={EditProduct} />
+          </Switch>
+        </div>
+      </Provider>
     </Router>
   );
 }
