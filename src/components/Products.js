9f55115e726804c81fe41299e04c51a99a97e0c2
geravr <gera.villa21@gmail.com>
2020-03-08 21:34:08 -0600

    show loading message

--- src/components/Products.js
+++ src/components/Products.js
@@ -18,12 +18,16 @@ const Products = () => {
     }, [])
 
     // Obtener el state desde redux
-    const products = useSelector ( state => state.products.products );
+    const products = useSelector (state => state.products.products);
+    const error = useSelector(state => state.products.error);
+    const loading = useSelector(state => state.products.loading);
 
     return ( 
         <Fragment>
             <h2 className="text-center my-5">Listado de productos</h2>
 
+            { error ? <p className="font-weight-bold alert alert-danger text-center mt-4">Hubo un error al cargar los productos</p> : null}
+            { loading ? <p className="text-center">Cargando...</p> : null}
             <table className="table table-striped">
                 <thead className="bg-primary table-dark">
                     <tr>
